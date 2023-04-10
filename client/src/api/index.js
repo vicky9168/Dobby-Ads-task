7e2bdc94a981ced59d5b985d368f178b434240e4
vicky9168 <dholevivek1111@gmail.com>
2023-04-11 02:59:05 +0530

    first commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -11,4 +11,3 @@ const API=axios.create({baseURL:'http://localhost:5000'})
 export const signUp=(authData)=> API.post('/user/signup',authData);
 export const logIn=(authData)=> API.post('/user/login',authData);
 
-// export const fetchAllUsers = () => API.get('/user/getAllUsers');
